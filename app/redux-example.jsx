894401175585e58fd3b8bd5c2713bbf5ae9d58d9
Ekrem Kamberoglu <ekamberoglu@gmail.com>
2017-08-14 00:12:01 +0300

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,8 +2,16 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
 
-var reducer = (state = { name: 'Anonymous' }, action) => {
+var nextHobbyId = 1;
+var nextMovieId = 1;
+
+var reducer = (state = stateDefault, action) => {
     // state = state || { name: 'Anonymous' };
 
     switch (action.type) {
@@ -12,7 +20,39 @@ var reducer = (state = { name: 'Anonymous' }, action) => {
           ...state,
           name: action.name
         };
-
+      case 'ADD_HOBBY':
+        return {
+          ...state,
+          hobbies: [
+              ...state.hobbies,
+              {
+                id: nextHobbyId++,
+                hobby: action.hobby
+              }
+          ]
+        };
+      case 'REMOVE_HOBBY':
+        return {
+          ...state,
+          hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+        }
+      case 'ADD_MOVIE':
+        return {
+          ...state,
+          movies: [
+            ...state.movies,
+            {
+              id: nextMovieId++,
+              title: action.title,
+              genre: action.genre
+            }
+          ]
+        };
+      case 'REMOVE_MOVIE':
+        return {
+          ...state,
+          movies: state.movies.filter((movie) => movie.id !== action.id)
+        }
       default:
         return state;
     }
@@ -27,25 +67,54 @@ var store = redux.createStore(reducer, redux.compose(
 	window.devToolsExtension ? window.devToolsExtension() : f => f
 ));
 
-var action = {
-  type: 'CHANGE_NAME',
-  name: 'Ekrem'
-}
-
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 
 //unsubscribe();
 
-store.dispatch(action);
+store.dispatch({
+  type: 'CHANGE_NAME',
+  name: 'Ekrem'
+});
 
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Swimming'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Kayros'
 });
 
-//console.log('Name should be Ekrem', store.getState());
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Mad Max',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Summer',
+  genre: 'Romantic'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+});
